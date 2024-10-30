39d6dfe7cff2e9dc5eec0804577002afa0869542
devcontainerDuy <125916124+devcontainerDuy@users.noreply.github.com>
2024-10-30 22:56:13 +0700

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>helo chào tâm nha</>;
+  return (
+    <>
+      helo chào tâm nha heheheheheheeh <button onClick={() => setCount((count) => count + 1)}>Nhấn tau đi {count}</button>
+    </>
+  );
 }
 
 export default App;
